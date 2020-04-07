ebe8ab1d1c534e0d13f03b43aadfbb6f7dfbefbb
WouterGaykema <43369278+WouterGaykema@users.noreply.github.com>
2020-04-07 16:46:04 +0200

    Add files via upload

--- blastORFs.java
+++ blastORFs.java
@@ -1,23 +1,13 @@
 
-import org.xml.sax.*;
 import java.io.*;
 
 import static org.biojava.nbio.ws.alignment.qblast.BlastAlignmentParameterEnum.ENTREZ_QUERY;
-import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.PreparedStatement;
-import java.util.Arrays;
-import java.util.List;
+
+import java.sql.*;
+import java.util.ArrayList;
 
 import org.biojava.nbio.core.sequence.io.util.IOUtils;
 import org.biojava.nbio.ws.alignment.qblast.*;
-
-import javax.swing.text.Document;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
 
 
 public class blastORFs {
@@ -25,11 +15,12 @@ public class blastORFs {
      * The blastORFS class accepts a list of ORF sequences as input and outputs BLAST results
      *
      * @author  Wouter Gaykema
-     * @version 1.0
-     * @since   2020-JAN-10
+     * @version 2.0
+     * @since   2020-APR-4
      */
     private static final String BLAST_OUTPUT_FILE = "blastOutput.xml";    // file to save blast results to
-    private static final List<String> ORFS = Arrays.asList("MKWVTFISLLFLFSSAYSRGVFRRDAHKSEVAHRFKDLGEENFKALVLIAFAQYLQQCP");     // Blast query sequence
+    private static int Blast_id;
+    private static int ORF_Id;
 
     public static Connection getConnection() throws SQLException {
         /**
@@ -39,22 +30,6 @@ public class blastORFs {
                 "jdbc:mysql://remotemysql.com:3306/NRCP73s0H5",
                 "NRCP73s0H5", "T10HedMmpO");
         return conn;
-    }
-
-    public static void createTable() throws SQLException {
-        /**
-         * The createTable class creates a table to store BLAST results if it does not already exist
-         */
-        Connection conn = getConnection();
-        PreparedStatement create = conn.prepareStatement("CREATE TABLE IF NOT EXISTS ORF_BLAST_results(" +
-                "resultNum varchar(10), Hit_num int," +
-                " Hit_id varchar(12), Hit_def varchar(255), Hit_accesion varchar(10), Hit_len int, Hsp_num int, " +
-                "Hsp_bit_score Double, Hsp_score int, Hsp_evalue varchar(255), Hsp_query_from int, Hsp_query_to int, " +
-                "Hsp_hit_from int, Hsp_hit_to int, Hsp_query_frame int, Hsp_hit_frame int, Hsp_identity int, " +
-                "Hsp_positive int, Hsp_gaps int, Hsp_align_len int, Hsp_qseq varchar(255), Hsp_hseq varchar(255), " +
-                "Hsp_midline varchar(255), PRIMARY KEY(resultNum))");
-        create.execute();
-        conn.close();
     }
 
     private static String convertXMLFileToString(String ORFfile, int ORFnum)
@@ -73,111 +48,70 @@ public class blastORFs {
 
         Connection conn = getConnection();
 
-        int rowNum = 1;
         BufferedReader reader;
         reader = new BufferedReader(new FileReader(ORFfile));
         String line = reader.readLine();
         int n = 0;
-        StringBuilder NextORF = new StringBuilder("ORF " + ORFnum + ": \n");
-        int Hit_num = 0;
+        StringBuilder NextORF = new StringBuilder("");
         String Hsp_midline = null;
         String Hsp_hseq = null;
         String Hsp_qseq = null;
-        int Hsp_align_len = 0;
-        int Hsp_gaps = 0;
-        int Hsp_positive = 0;
         int Hsp_identity = 0;
-        int Hsp_hit_frame = 0;
-        int Hsp_query_frame = 0;
-        int Hsp_hit_to = 0;
-        int Hsp_hit_from = 0;
         int Hsp_query_to = 0;
         int Hsp_query_from = 0;
         String Hsp_evalue = null;
-        int Hsp_score = 0;
-        Double Hsp_bit_score = null;
-        int Hsp_num = 0;
-        int Hit_len = 0;
         String Hit_accesion = null;
         String Hit_def = null;
         String Hit_id = null;
+        int QueryCover = 0;
+        String hit_seq = null;
+        int Blast_id = 0;
+        int ORF_Id = 0;
+        Statement stmt = conn.createStatement();
+        String strSelect = "select max(BlastResultaten_ID) from BlastResultaten";
+        ResultSet rset = stmt.executeQuery(strSelect);
+        while(rset.next()) {
+            Blast_id = rset.getInt("max(BlastResultaten_ID)");
+        }
+        Blast_id += 1;
+        System.out.println(Blast_id);
+        Statement stmt2 = conn.createStatement();
+        String strSelect2 = "select max(ORF_ID) from ORF";
+        ResultSet rset2 = stmt2.executeQuery(strSelect2);
+        while(rset2.next()) {
+            ORF_Id = rset2.getInt("max(ORF_ID)");
+        }
         while (line != null) {
             NextORF.append(line).append("\n");
 
-            if (line.matches("^  <Hit_num>.*</Hit_num>$")) {
-                Hit_num = Integer.parseInt(line.substring(11, line.length() - 10));
-            } else if (line.matches("^  <Hit_id>.*</Hit_id>$")) {
-                Hit_id = line.substring(10, line.length() - 9);
-            } else if (line.matches("^  <Hit_def>.*</Hit_def>$")) {
+             if (line.matches("^  <Hit_def>.*</Hit_def>$")) {
                 Hit_def = line.substring(11, line.length() - 10);
-                if( Hit_def.length()>=255){
-                    Hit_def = (Hit_def.substring(0, 251) + "...");
-                }
             } else if (line.matches("^  <Hit_accession>.*</Hit_accession>$")) {
                 Hit_accesion = line.substring(17, line.length() - 16);
-            } else if (line.matches("^  <Hit_len>.*</Hit_len>$")) {
-                Hit_len = Integer.parseInt(line.substring(11, line.length() - 10));
-            } else if (line.matches("^      <Hsp_num>.*</Hsp_num>$")) {
-                Hsp_num = Integer.parseInt(line.substring(15, line.length() - 10));
-            } else if (line.matches("^      <Hsp_bit-score>.*</Hsp_bit-score>$")) {
-                Hsp_bit_score = Double.parseDouble(line.substring(21, line.length() - 16));
-            } else if (line.matches("^      <Hsp_score>.*</Hsp_score>$")) {
-                Hsp_score = Integer.parseInt(line.substring(17, line.length() - 12));
             } else if (line.matches("^      <Hsp_evalue>.*</Hsp_evalue>$")) {
                 Hsp_evalue = line.substring(18, line.length() - 13);
-                if( Hsp_evalue.length()>=255){
-                    Hsp_evalue = (Hsp_evalue.substring(0, 251) + "...");
-                }
             } else if (line.matches("^      <Hsp_query-from>.*</Hsp_query-from>$")) {
                 Hsp_query_from = Integer.parseInt(line.substring(22, line.length() - 17));
             } else if (line.matches("^      <Hsp_query-to>.*</Hsp_query-to>$")) {
                 Hsp_query_to = Integer.parseInt(line.substring(20, line.length() - 15));
-            } else if (line.matches("^      <Hsp_hit-from>.*</Hsp_hit-from>$")) {
-                Hsp_hit_from = Integer.parseInt(line.substring(20, line.length() - 15));
-            } else if (line.matches("^      <Hsp_hit-to>.*</Hsp_hit-to>$")) {
-                Hsp_hit_to = Integer.parseInt(line.substring(18, line.length() - 13));
-            } else if (line.matches("^      <Hsp_query-frame>.*</Hsp_query-frame>$")) {
-                Hsp_query_frame = Integer.parseInt(line.substring(23, line.length() - 18));
-            } else if (line.matches("^      <Hsp_hit-frame>.*</Hsp_hit-frame>$")) {
-                Hsp_hit_frame = Integer.parseInt(line.substring(21, line.length() - 16));
-            } else if (line.matches("^      <Hsp_identity>.*</Hsp_identity>$")) {
-                Hsp_identity = Integer.parseInt(line.substring(20, line.length() - 15));
-            } else if (line.matches("^      <Hsp_positive>.*</Hsp_positive>$")) {
-                Hsp_positive = Integer.parseInt(line.substring(20, line.length() - 15));
-            } else if (line.matches("^      <Hsp_gaps>.*</Hsp_gaps>$")) {
-                Hsp_gaps = Integer.parseInt(line.substring(16, line.length() - 11));
-            } else if (line.matches("^      <Hsp_align-len>.*</Hsp_align-len>$")) {
-                Hsp_align_len = Integer.parseInt(line.substring(21, line.length() - 16));
             } else if (line.matches("^      <Hsp_qseq>.*</Hsp_qseq>$")) {
                 Hsp_qseq = line.substring(16, line.length() - 11);
-                if( Hsp_qseq.length()>=255){
-                    Hsp_qseq = (Hsp_qseq.substring(0, 251) + "...");
-                }
             } else if (line.matches("^      <Hsp_hseq>.*</Hsp_hseq>$")) {
                 Hsp_hseq = line.substring(16, line.length() - 11);
-                if( Hsp_hseq.length()>=255){
-                    Hsp_hseq = (Hsp_hseq.substring(0, 251) + "...");
-                }
             } else if (line.matches("^      <Hsp_midline>.*</Hsp_midline>$")) {
                 Hsp_midline = line.substring(19, line.length() - 14);
-                if( Hsp_midline.length()>=255){
-                    Hsp_midline = (Hsp_midline.substring(0, 251) + "...");
-                }
+            } else if (line.matches("^      <Hsp_identity>.*</Hsp_identity>$")) {
+                Hsp_identity = Integer.parseInt(line.substring(20, line.length() - 15));
             } else if (line.matches("^</Hit>$")) {
-                String resultNum = String.valueOf(ORFnum) + "." + String.valueOf(Hit_num);//TODO dit is de primary key dus als we meerdere dingen willen invullen moet hier ook de naam van het ingevoerde genoom bij
-                PreparedStatement posted = conn.prepareStatement("INSERT INTO ORF_BLAST_results (resultNum, " +
-                        "Hit_num, Hit_id, Hit_def, Hit_accesion, Hit_len, Hsp_num, Hsp_bit_score, Hsp_score, " +
-                        "Hsp_evalue, Hsp_query_from, Hsp_query_to, Hsp_hit_from, Hsp_hit_to, Hsp_query_frame, " +
-                        "Hsp_hit_frame, Hsp_identity, Hsp_positive, Hsp_gaps, Hsp_align_len, Hsp_qseq, Hsp_hseq, " +
-                        "Hsp_midline) VALUES ('" + resultNum + "', '" + Hit_num + "', '" + Hit_id + "', " +
-                        "'" + Hit_def + "', '" + Hit_accesion + "', '" + Hit_len + "', '" + Hsp_num + "', " +
-                        "'" + Hsp_bit_score + "', '" + Hsp_score + "', '" + Hsp_evalue + "', '" + Hsp_query_from + "', " +
-                        "'" + Hsp_query_to + "', '" + Hsp_hit_from + "', '" + Hsp_hit_to + "', " +
-                        "'" + Hsp_query_frame + "', '" + Hsp_hit_frame + "', '" + Hsp_identity + "', " +
-                        "'" + Hsp_positive + "', '" + Hsp_gaps + "', '" + Hsp_align_len + "', '" + Hsp_qseq + "', " +
-                        "'" + Hsp_hseq + "', '" + Hsp_midline + "')");
+                QueryCover = (Hsp_query_to - Hsp_query_from) + 1;
+                hit_seq = Hsp_qseq + "\n" + Hsp_midline + "\n" + Hsp_hseq;
+                PreparedStatement posted = conn.prepareStatement("INSERT INTO BlastResultaten (E_value, " +
+                        "Query_cover, Percentage_identity, Accessiecode, Titel, EiwitSequentie, ORF_ORF_ID, " +
+                        "BlastResultaten_ID) VALUES ('" + Hsp_evalue + "', '" + QueryCover + "', '" + Hsp_identity + "', " +
+                        "'" + Hit_accesion + "', '" + Hit_def + "', '" + hit_seq + "', '" + ORF_Id + "', " +
+                        "'" + Blast_id + "')");
                 posted.execute();
-                rowNum += 1;
+                Blast_id += 1;
             }
             // read next line
             line = reader.readLine();
@@ -254,22 +188,77 @@ public class blastORFs {
         }
     }
 
-    public static void main(String[] args) throws SQLException, IOException {
+    public static String main(ArrayList<String> ORFS, int ORFnum) throws SQLException, IOException {
         /**
          * The main function ties together all the other functions
          */
-        createTable();
-        int ORFnum = 1;
         String BlastString = "";
-        List<String> ORFS = Arrays.asList("MKWVTFISLLFLFSSAYSRGVFRRDAHKSEVAHRFKDLGEENFKALVLIAFAQYLQQCP");     // Blast query sequence//TODO dit zijn de ORF sequenties
-        for (int i = 0; i< ORFS.size(); i++) {
-            String ORFfile = GenerateXML(ORFS.get(i));
-            String NextORF = convertXMLFileToString(ORFfile, ORFnum);
-            BlastString += NextORF;
-            ORFnum += 1;
+        String ORFnucleo = "";
+        ORFnucleo = ORFS.get(ORFnum).substring(3, ORFnucleo.length() - 3);
+        StringBuilder ORFString = new StringBuilder();
+        for(int i = 0; i < ORFnucleo.length()-2; i = i+3){
+            String codon = String.valueOf(ORFnucleo.charAt(i)) + String.valueOf(ORFnucleo.charAt(i+1)) +
+                    String.valueOf(ORFnucleo.charAt(i+2));
+            if(codon.equalsIgnoreCase("gct") || codon.equalsIgnoreCase("gcc") ||
+                    codon.equalsIgnoreCase("gca")|| codon.equalsIgnoreCase("gcg")){
+                ORFString.append("A");
+            } else if(codon.equalsIgnoreCase("cgt") || codon.equalsIgnoreCase("cgc") ||
+                    codon.equalsIgnoreCase("cga")|| codon.equalsIgnoreCase("cgg") ||
+                    codon.equalsIgnoreCase("aga") || codon.equalsIgnoreCase("agg")){
+                ORFString.append("R");
+            } else if(codon.equalsIgnoreCase("aat") || codon.equalsIgnoreCase("aac")){
+                ORFString.append("N");
+            } else if(codon.equalsIgnoreCase("gat") || codon.equalsIgnoreCase("gac")){
+                ORFString.append("D");
+            } else if(codon.equalsIgnoreCase("tgt") || codon.equalsIgnoreCase("tgc")){
+                ORFString.append("C");
+            } else if(codon.equalsIgnoreCase("caa") || codon.equalsIgnoreCase("cag")){
+                ORFString.append("Q");
+            } else if(codon.equalsIgnoreCase("gaa") || codon.equalsIgnoreCase("gag")){
+                ORFString.append("E");
+            } else if(codon.equalsIgnoreCase("ggt") || codon.equalsIgnoreCase("ggc") ||
+                    codon.equalsIgnoreCase("gga") || codon.equalsIgnoreCase("ggg")){
+                ORFString.append("G");
+            } else if(codon.equalsIgnoreCase("cat") || codon.equalsIgnoreCase("cac")){
+                ORFString.append("H");
+            } else if(codon.equalsIgnoreCase("att") || codon.equalsIgnoreCase("atc") ||
+                    codon.equalsIgnoreCase("ata")){
+                ORFString.append("I");
+            } else if(codon.equalsIgnoreCase("ctt") || codon.equalsIgnoreCase("ctc") ||
+                    codon.equalsIgnoreCase("cta")|| codon.equalsIgnoreCase("ctg") ||
+                    codon.equalsIgnoreCase("tta") || codon.equalsIgnoreCase("ttg")){
+                ORFString.append("L");
+            } else if(codon.equalsIgnoreCase("aaa") || codon.equalsIgnoreCase("aag")){
+                ORFString.append("K");
+            } else if(codon.equalsIgnoreCase("atg")){
+                ORFString.append("M");
+            } else if(codon.equalsIgnoreCase("ttt") || codon.equalsIgnoreCase("ttc")){
+                ORFString.append("F");
+            } else if(codon.equalsIgnoreCase("cct") || codon.equalsIgnoreCase("ccc") ||
+                    codon.equalsIgnoreCase("cca") || codon.equalsIgnoreCase("ccg")){
+                ORFString.append("P");
+            } else if(codon.equalsIgnoreCase("tct") || codon.equalsIgnoreCase("tcc") ||
+                    codon.equalsIgnoreCase("tca")|| codon.equalsIgnoreCase("tcg") ||
+                    codon.equalsIgnoreCase("agt") || codon.equalsIgnoreCase("agc")){
+                ORFString.append("S");
+            } else if(codon.equalsIgnoreCase("act") || codon.equalsIgnoreCase("acc") ||
+                    codon.equalsIgnoreCase("aca") || codon.equalsIgnoreCase("acg")){
+                ORFString.append("T");
+            } else if(codon.equalsIgnoreCase("tgg")){
+                ORFString.append("W");
+            } else if(codon.equalsIgnoreCase("tat") || codon.equalsIgnoreCase("tac")){
+                ORFString.append("Y");
+            } else if(codon.equalsIgnoreCase("gtt") || codon.equalsIgnoreCase("gtc") ||
+                    codon.equalsIgnoreCase("gta") || codon.equalsIgnoreCase("gtg")){
+                ORFString.append("V");
+            }
         }
-        System.out.println(BlastString);
-
+        String ORFfile = GenerateXML(ORFString.toString());
+        System.out.println(ORFString);
+        String NextORF = convertXMLFileToString(ORFfile.toString(), ORFnum);
+        BlastString += NextORF + "\n\n";
+        BlastString += "results saved in " + ORFfile;
+        return BlastString;
     }
 
 }
